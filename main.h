0b3d004284e8db13dd0903b8073bbd16e5092169
Amina0205 <amina_elkharraz@hotmail.com>
2023-04-25 13:55:27 -0700

    _printf.c

--- main.h
+++ main.h
@@ -36,7 +36,7 @@ struct fmt
  * @fmt: The format
  * @fm_t: The function associated
  */
-typedef struct fmt éfmt_t;
+typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[], int flags, int width, int precition, int size);
@@ -65,10 +65,10 @@ int print_non_printable(va_list types, char buffer[], int flags, int width, int 
 int print_pointer(va_list types, char buffer[], int flags, int width, int precision, int size);
 
 /* Functions to handle other specifiers */
-int get_flags(const char *format, int *i).
-int get_width(cinst char *format, int *i, va_list list);
-int get precision(const char *format, int *i, va_list list);
-int get_size(const char *format, int i);
+int get_flags(const char *format, int *i);
+int get_width(const char *format, int *i, va_list list);
+int get_precision(const char *format, int *i, va_list list);
+int get_size(const char *format, int *i);
 
 /* Function to print string in reverse */
 int print_reverse(va_list types, char buffer[], int flags, int width, int precision, int size);
@@ -80,7 +80,7 @@ int print_rot13string(va_list types, char buffer[], int flags, int width, int pr
 int handle_write_char(char c, char buffer[], int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[], int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision, int length, char padd, char extra_c);
-int write_pointer(char buffer[], int int, int length, int width, int flags, char padd, char extra_c, int padd_start);
+int write_pointer(char buffer[], int ind, int length, int width, int flags, char padd, char extra_c, int padd_start);
 
 int write_unsgnd(int is_negative, int ind, char buffer[], int flags, int width, int precision, int size);
 
@@ -89,7 +89,7 @@ int is_printable(char);
 int append_hexa_code(char, char[], int);
 int is_digit(char);
 
-lont int convert_size_number(lont int num, int size);
-lont int convert_size_unsgnd(unsigned long int num, int size);
+long int convert_size_number(long int num, int size);
+long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /*MAIN_H*/
